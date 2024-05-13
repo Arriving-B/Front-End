4263b617a59be6f59f42d72a7c29f53bd1751223
이형석 <2hyeongseok@gmail.com>
2024-05-13 21:22:56 +0900

    feat: feedback 퍼블리싱 및 기능구현
    + 짜잘한 세부디자인

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,54 +1,55 @@
-import gpsIconOff from "assets/header/Gps_icon_off.png";
-import gpsIconOn from "assets/header/Gps_icon_on.png";
-import mainIcon from "assets/header/main_icon.png";
-import resetIcon from "assets/header/reset_icon.png";
-import searchIcon from "assets/header/search_icon.png";
-import settingButton from "assets/header/setting_icon.png";
-import { useState } from "react";
-import styled from "styled-components";
+import gpsIconOff from 'assets/header/Gps_icon_off.png'
+import gpsIconOn from 'assets/header/Gps_icon_on.png'
+import mainIcon from 'assets/header/main_icon.png'
+import resetIcon from 'assets/header/reset_icon.png'
+import searchIcon from 'assets/header/search_icon.png'
+import settingButton from 'assets/header/setting_icon.png'
+import React, { useState } from 'react'
+import styled from 'styled-components'
+import Swal from 'sweetalert2'
 
 const HeaderComponent = styled.div`
   position: fixed;
-  background-color: #272725;
+  background-color: #252527;
   width: 100%;
-  height: 80px;
+  height: 10vh;
   display: flex;
   flex-direction: row;
   align-items: center;
   top: 0;
-`;
+`
 const HeaderLeftComponent = styled.div`
   width: 50%;
   margin-right: 5%;
-  margin-left: clamp(5px, 5%, 5%);
+  margin-left: clamp(0px, 2%, 5%);
   display: flex;
   flex-direction: row;
   align-items: center;
   justify-items: start;
-`;
+`
 const MainIcon = styled.span`
   position: relative;
   width: 50px;
   height: 50px;
-  margin: 0 30px 0 0px;
+  margin: 0 30px 0 10%;
   & img {
     width: 50px;
     height: 50px;
   }
-`;
+`
 
 const LocationBox = styled.div`
   position: relative;
   background-color: white;
   width: 220px;
-  height: 40px;
+  height: 5.8vh;
   border-radius: 30px;
   display: flex;
   flex-direction: row;
   align-items: center;
   margin: 0 clamp(5px, 25px, 25px) 0 clamp(0px, 0px, 5px);
   text-align: center;
-`;
+`
 const LocationTextBox = styled.div`
   width: 175px;
   height: 40px;
@@ -56,19 +57,19 @@ const LocationTextBox = styled.div`
   color: black;
   font-size: x-small;
   text-align: center;
-`;
+`
 const LittleLocationTextBox = styled.p`
   height: 20px;
   margin-block-start: 0;
   margin: 0;
   font-size: x-small;
-`;
+`
 const BigLocationTextBox = styled.h6`
   height: 20px;
   margin-block-start: 0;
   margin: 0;
   font-size: small;
-`;
+`
 const ResetButton = styled.span`
   margin-right: 8px;
   width: 25px;
@@ -79,11 +80,47 @@ const ResetButton = styled.span`
   display: flex;
   align-items: center;
   justify-content: center;
+  cursor: pointer;
   & img {
     width: 25px;
     height: 25px;
   }
-`;
+  &:hover {
+    animation-duration: 0.8s;
+    animation-name: slidein;
+
+    @keyframes slidein {
+      15% {
+        transform: rotate(40deg);
+      }
+
+      45% {
+        transform: rotate(-40deg);
+      }
+
+      60% {
+        transform: rotate(20deg);
+      }
+
+      75% {
+        transform: rotate(-20deg);
+      }
+
+      90% {
+        transform: rotate(10deg);
+      }
+
+      100% {
+        transform: rotate(0);
+      }
+    }
+  }
+  &:active {
+    transition: all 4s;
+    transform: rotate(-360deg);
+  }
+`
+
 const GpsButton = styled.span`
   width: 30px;
   height: 30px;
@@ -93,11 +130,12 @@ const GpsButton = styled.span`
   display: flex;
   align-items: center;
   justify-content: center;
+  cursor: pointer;
   & img {
     width: 30px;
     height: 30px;
   }
-`;
+`
 
 const HeaderRightComponent = styled.div`
   width: 50%;
@@ -105,49 +143,129 @@ const HeaderRightComponent = styled.div`
   flex-direction: row;
   align-items: center;
   border-left: 150px;
-`;
+`
 
 const SearchBox = styled.div`
   position: relative;
   background-color: white;
   width: clamp(400px, 70%, 700px);
-  height: 40px;
+  height: 6vh;
   border-radius: 30px;
   display: flex;
   flex-direction: row;
   align-items: center;
   justify-content: start;
   margin-left: clamp(10px, 5%, 80px);
-`;
-const SearchTextBox = styled.h5`
+`
+const SearchTextBox = styled.input`
   width: clamp(300px, 100%, 600px);
   text-align: start;
-  margin: 0 10px 0 20px;
-  color: grey;
-`;
+  margin: 0 10px 0 12px;
+  background-color: white;
+  color: gray;
+  border-color: transparent;
+  outline: none;
+`
 const SearchIcon = styled.img`
   position: relative;
   width: 20px;
   height: 20px;
   right: 8px;
-`;
+  cursor: pointer;
+`
 
-const SettingButton = styled.span`
+const SettingButton = styled.div`
   position: relative;
   width: 30px;
   height: 30px;
   margin-left: 60px;
   margin-right: 10px;
+  cursor: pointer;
   & img {
     width: 30px;
     height: 30px;
   }
-`;
+  &:hover {
+    animation-duration: 2s;
+    animation-name: slidein;
 
-function Header() {
-  const [isGps, setIsGps] = useState<boolean>(true);
+    @keyframes slidein {
+      100% {
+        transform: rotate(720deg);
+      }
+    }
+  }
+`
 
-  const handleGps = () => setIsGps(!isGps);
+const SideMenuBar = styled.div<{ rightlocation: number }>`
+  width: 240px;
+  height: 14%;
+  background: #222;
+  position: fixed;
+  top: 10vh;
+  right: ${(props) => props.rightlocation}px;
+  z-index: 1;
+  transition: all 0.35s;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+const SendFeedbackButton = styled.button`
+  width: 50%;
+  height: 30%;
+  background: white;
+  color: skyblue;
+  font-size: 10px;
+`
+
+function Header(): React.JSX.Element {
+  const [isGps, setIsGps] = useState<boolean>(true)
+  const handleGps = () => setIsGps(!isGps)
+  const [inputText, setInputText] = useState('')
+  const [isSideMenuOpen, setIsSideMenuOpen] = useState<boolean>(false)
+  const researchNearestStation = () => {
+    alert('가장 가까운 정류장 새로고침')
+  }
+
+  const handleSearch = () => {
+    alert(`${inputText} 입력완료`)
+  }
+  const handleInput = (e: React.KeyboardEvent) => {
+    if (e.key === 'Enter') {
+      handleSearch()
+    }
+  }
+  const getFeedBack = () => {
+    Swal.fire({
+      title: 'Submit your FeedBack',
+      input: 'text',
+      inputAttributes: {
+        autocapitalize: 'off',
+      },
+      showCancelButton: true,
+      confirmButtonText: 'Look up',
+      showLoaderOnConfirm: true,
+      allowOutsideClick: () => !Swal.isLoading(),
+    }).then((result) => {
+      const Toast = Swal.mixin({
+        toast: true,
+        position: 'bottom-right',
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+          toast.addEventListener('mouseenter', Swal.stopTimer)
+          toast.addEventListener('mouseleave', Swal.resumeTimer)
+        },
+      })
+
+      Toast.fire({
+        icon: 'success',
+        title: `${result.value}이 정상적으로 입력됨`,
+      })
+    })
+  }
 
   return (
     <HeaderComponent>
@@ -160,7 +278,7 @@ function Header() {
             <LittleLocationTextBox>가장 가까운 정류장</LittleLocationTextBox>
             <BigLocationTextBox>어쩔티비</BigLocationTextBox>
           </LocationTextBox>
-          <ResetButton>
+          <ResetButton onClick={researchNearestStation}>
             <img src={resetIcon} />
           </ResetButton>
         </LocationBox>
@@ -170,15 +288,23 @@ function Header() {
       </HeaderLeftComponent>
       <HeaderRightComponent>
         <SearchBox>
-          <SearchTextBox>도착지점 또는 버스 검색</SearchTextBox>
-          <SearchIcon src={searchIcon} />
+          <SearchTextBox
+            type="text"
+            placeholder="도착지점 또는 버스 검색"
+            onChange={(e) => setInputText(e.target.value)}
+            onKeyDown={handleInput}
+          />
+          <SearchIcon src={searchIcon} onClick={handleSearch} />
         </SearchBox>
-        <SettingButton>
+        <SettingButton onClick={() => setIsSideMenuOpen(!isSideMenuOpen)}>
           <img src={settingButton}></img>
         </SettingButton>
       </HeaderRightComponent>
+      <SideMenuBar rightlocation={isSideMenuOpen ? 0 : -300}>
+        <SendFeedbackButton onClick={getFeedBack}>피드백 전송</SendFeedbackButton>
+      </SideMenuBar>
     </HeaderComponent>
-  );
+  )
 }
 
-export default Header;
+export default Header
